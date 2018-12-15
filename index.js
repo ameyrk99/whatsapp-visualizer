1f906db76916e182287705fc180c5907cf7f40c1
Amey Khairnar <ameykhairnar99@gmail.com>
2018-12-15 18:13:23 -0500

    Changed most used words to most used non-stop words, minimum length of word to 3

--- index.js
+++ index.js
@@ -135,13 +135,60 @@ getData = (usrs, lines) => {
     return [msgs, wrds, mda, words, times];
 }
 
+const stopwords = ["a's", 'able', 'about', 'above', 'according', 'accordingly', 'across', 'actually', 'after', 
+    'afterwards', 'again', 'against', "ain't", 'all', 'allow', 'allows', 'almost', 'alone', 'along', 'already', 
+    'also', 'although', 'always', 'am', 'among', 'amongst', 'an', 'and', 'another', 'any', 'anybody', 'anyhow', 
+    'anyone', 'anything', 'anyway', 'anyways', 'anywhere', 'apart', 'appear', 'appreciate', 'appropriate', 'are', 
+    "aren't", 'around', 'as', 'aside', 'ask', 'asking', 'associated', 'at', 'available', 'away', 'awfully', 'be', 
+    'became', 'because', 'become', 'becomes', 'becoming', 'been','before', 'beforehand', 'behind', 'being', 
+    'believe', 'below', 'beside', 'besides', 'best', 'better', 'between', 'beyond', 'both', 'brief', 'but', 
+    'by', "c'mon", "c's", 'came', 'can', "can't", 'cannot', 'cant', 'cause', 'causes', 'certain', 'certainly', 
+    'changes', 'clearly', 'co', 'com', 'come', 'comes', 'concerning', 'consequently', 'consider', 'considering', 
+    'contain', 'containing', 'contains', 'corresponding', 'could', "couldn't", 'course', 'currently', 'definitely', 
+    'described', 'despite', 'did', "didn't", 'different', 'do', 'does', "doesn't", 'doing', "don't", 'done', 'down', 
+    'downwards', 'during', 'each', 'edu', 'eg', 'eight', 'either', 'else', 'elsewhere', 'enough', 'entirely', 
+    'especially', 'et', 'etc', 'even', 'ever', 'every', 'everybody', 'everyone', 'everything', 'everywhere', 'ex', 
+    'exactly', 'example', 'except', 'far', 'few', 'fifth', 'first', 'five', 'followed', 'following', 'follows', 
+    'for', 'former', 'formerly', 'forth', 'four', 'from', 'further', 'furthermore', 'get', 'gets', 'getting', 'given', 
+    'gives', 'go', 'goes', 'going', 'gone', 'got', 'gotten', 'greetings', 'had', "hadn't", 'happens', 'hardly', 'has', 
+    "hasn't", 'have', "haven't", 'having','he', "he's", 'hello', 'help', 'hence', 'her', 'here', "here's", 'hereafter', 
+    'hereby', 'herein', 'hereupon', 'hers', 'herself', 'hi', 'him', 'himself', 'his', 'hither', 'hopefully', 'how', 
+    'howbeit', 'however', "i'd", "i'll", "i'm", "i've", 'ie', 'if', 'ignored', 'immediate', 'in', 'inasmuch', 'inc', 
+    'indeed', 'indicate', 'indicated', 'indicates', 'inner', 'insofar', 'instead', 'into', 'inward', 'is', "isn't", 
+    'it', "it'd", "it'll", "it's", 'its', 'itself', 'just', 'keep', 'keeps', 'kept', 'know', 'known', 'knows', 'last', 
+    'lately', 'later', 'latter', 'latterly', 'least', 'less', 'lest', 'let', "let's", 'like', 'liked', 'likely', 
+    'little', 'look', 'looking', 'looks', 'ltd', 'mainly', 'many', 'may', 'maybe', 'me', 'mean', 'meanwhile', 'merely', 
+    'might', 'more', 'moreover', 'most', 'mostly', 'much', 'must', 'my', 'myself', 'name', 'namely', 'nd', 'near', 
+    'nearly', 'necessary', 'need', 'needs', 'neither', 'never', 'nevertheless', 'new', 'next', 'nine', 'no', 'nobody', 
+    'non', 'none', 'noone', 'nor', 'normally', 'not', 'nothing', 'novel', 'now', 'nowhere', 'obviously', 'of', 'off', 
+    'often', 'oh', 'ok', 'okay', 'old', 'on', 'once', 'one', 'ones', 'only', 'onto', 'or','other', 'others', 'otherwise', 
+    'ought', 'our', 'ours', 'ourselves', 'out', 'outside', 'over', 'overall', 'own', 'particular', 'particularly', 'per', 
+    'perhaps', 'placed', 'please', 'plus', 'possible', 'presumably', 'probably', 'provides', 'que', 'quite', 'qv', 'rather', 
+    'rd', 're', 'really', 'reasonably', 'regarding', 'regardless', 'regards', 'relatively', 'respectively', 'right', 
+    'said', 'same', 'saw', 'say', 'saying', 'says', 'second', 'secondly', 'see', 'seeing', 'seem', 'seemed', 'seeming', 
+    'seems', 'seen', 'self', 'selves', 'sensible', 'sent', 'serious', 'seriously', 'seven', 'several', 'shall', 'she', 
+    'should', "shouldn't", 'since', 'six', 'so', 'some', 'somebody', 'somehow', 'someone', 'something', 'sometime', 
+    'sometimes', 'somewhat', 'somewhere', 'soon', 'sorry', 'specified', 'specify', 'specifying', 'still', 'sub', 'such', 
+    'sup', 'sure', "t's", 'take', 'taken', 'tell', 'tends', 'th', 'than', 'thank', 'thanks', 'thanx', 'that', "that's", 
+    'thats', 'the', 'their','theirs', 'them', 'themselves', 'then', 'thence', 'there', "there's", 'thereafter', 'thereby', 
+    'therefore', 'therein', 'theres', 'thereupon', 'these', 'they', "they'd", "they'll", "they're", "they've", 'think', 
+    'third', 'this', 'thorough', 'thoroughly', 'those', 'though', 'three', 'through', 'throughout', 'thru', 'thus', 'to', 
+    'together', 'too', 'took', 'toward', 'towards', 'tried', 'tries', 'truly', 'try', 'trying', 'twice', 'two', 'un', 'under', 
+    'unfortunately', 'unless', 'unlikely', 'until', 'unto', 'up', 'upon', 'us', 'use', 'used', 'useful', 'uses', 'using', 
+    'usually', 'value', 'various', 'very', 'via', 'viz', 'vs', 'want', 'wants', 'was', "wasn't", 'way', 'we', "we'd", "we'll", 
+    "we're", "we've", 'welcome', 'well', 'went', 'were', "weren't", 'what', "what's", 'whatever', 'when', 'whence', 'whenever', 
+    'where', "where's", 'whereafter', 'whereas', 'whereby', 'wherein', 'whereupon', 'wherever', 'whether', 'which', 'while', 
+    'whither', 'who', "who's", 'whoever', 'whole','whom', 'whose', 'why', 'will', 'willing', 'wish', 'with', 'within', 
+    'without', "won't", 'wonder', 'would', "wouldn't", 'yes', 'yet', 'you', "you'd", "you'll", "you're", "you've", 'your', 
+    'yours','yourself', 'yourselves', 'zero'];
+
 getTopUsedWords = (words, minLength) => {
     /**
      * Get the array of most top 10 used words above length minLength, and the number of times each of them is used.
      */
     let satWords = [];
     for(let i = 0; i < words.length; i++) {
-        if(words[i].length >= minLength) {
+        if(words[i].length >= minLength && !stopwords.includes(words[i])) {
             satWords.push(words[i]);
         }
     }
@@ -161,7 +208,13 @@ getTopUsedWords = (words, minLength) => {
         return b[1] - a[1];
     });
 
-    const top = sortable.slice(0, 10);
+    let top = [];
+    try {
+        top = sortable.slice(0, 10);
+    } catch(err) {
+        top = sortable;
+    }
+
     let lWords = [], lCount = [];
     for(let i = 0; i < top.length; i++) {
         lWords.push(top[i][0]);
@@ -227,7 +280,7 @@ main = (doc) => {
     const data = getData(users, lines);
     const crr_times = data[4];
 
-    const topWords = getTopUsedWords(data[3], 4);
+    const topWords = getTopUsedWords(data[3], 3);
 
     const times = getTimes(dates, crr_times);
 
@@ -417,7 +470,7 @@ graphTopWords = (anadata) => {
         data: {
             labels: words[0],
             datasets: [{
-                label: 'Top Used Words (length 4+)',
+                label: 'Top Used Words (length 3+)',
                 data: words[1],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
